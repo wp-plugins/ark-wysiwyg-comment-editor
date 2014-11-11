bf8383669c5f8aee8c7bf1ac2a61430a014da329
AlexKar <AlexKar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-11 05:25:09 +0000

    Adding ark-commenteditor version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/ark-wysiwyg-comment-editor/trunk@1023609 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugins/arkemoticons/plugin.js
@@ -0,0 +1,61 @@
+/* arkemoticons v 1.0
+ * (c) Alexander Karataev 10/2014
+ * ddw2@yandex.ru
+ */
+
+tinymce.PluginManager.add('arkemoticons', function(editor, url) {
+var arkagent = [
+["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+["11", "12", "13", "14", "15", "16", "17", "18", "19", "20"],
+["21", "22", "23", "24", "25", "26", "27", "28", "29", "30"],
+["31", "32", "33", "34", "35", "36", "37", "38", "39", "40"],
+["41", "42", "43", "44", "45", "46", "47"]
+];
+
+function getHtml() {
+var arkagentHtml;
+
+arkagentHtml = '<table role="list" class="mce-grid" style="width:400px !important; height:200px !important;">';
+
+tinymce.each(arkagent, function(row) {
+arkagentHtml += '<tr>';
+
+tinymce.each(row, function(icon) {
+var arkagentUrl = url + '/img/' + icon + '.gif';
+
+arkagentHtml += '<td><a href="#" data-mce-url="' + arkagentUrl + '" data-mce-alt="' + icon + '" tabindex="-1" ' +
+'role="option" aria-label="' + icon + '"><img src="' +
+arkagentUrl + '" style="width:32;" role="presentation" /></a></td>';
+});
+
+arkagentHtml += '</tr>';
+});
+
+arkagentHtml += '</table>';
+
+return arkagentHtml;
+}
+
+editor.addButton('arkemoticons', {
+type: 'panelbutton',
+panel: {
+role: 'application',
+autohide: true,
+html: getHtml,
+onclick: function(e) {
+var linkElm = editor.dom.getParent(e.target, 'a');
+
+if (linkElm) {
+editor.insertContent(
+'<img src="' + linkElm.getAttribute('data-mce-url') + '" alt="' + linkElm.getAttribute('data-mce-alt') + '" />'
+);
+
+//this.hide();
+}
+}
+},
+tooltip: 'Smile Agent',
+image: url + '/img/smile_agent.png'
+});
+});
+
---
+++ plugins/arkemoticonssk/plugin.js
@@ -0,0 +1,60 @@
+/* arkemoticons v 1.0
+ * (c) Alexander Karataev 10/2014
+ * ddw2@yandex.ru
+ */
+
+tinymce.PluginManager.add('arkemoticonssk', function(editor, url) {
+var arkskype = [
+["01", "02", "03", "04", "05", "06"],
+["07", "08", "09", "10", "11", "12"],
+["13", "14", "15", "16", "17", "18"],
+["19", "20", "21", "22"]
+];
+
+function getHtml() {
+var arkskypeHtml;
+
+arkskypeHtml = '<table role="list" class="mce-grid" style="width:200px !important; height:100px !important;">';
+
+tinymce.each(arkskype, function(row) {
+arkskypeHtml += '<tr>';
+
+tinymce.each(row, function(icon) {
+var arkskypeUrl = url + '/img/' + icon + '.gif';
+
+arkskypeHtml += '<td><a href="#" data-mce-url="' + arkskypeUrl + '" data-mce-alt="' + icon + '" tabindex="-1" ' +
+'role="option" aria-label="' + icon + '"><img src="' +
+arkskypeUrl + '" role="presentation" /></a></td>';
+});
+
+arkskypeHtml += '</tr>';
+});
+
+arkskypeHtml += '</table>';
+
+return arkskypeHtml;
+}
+
+editor.addButton('arkemoticonssk', {
+type: 'panelbutton',
+panel: {
+role: 'application',
+autohide: true,
+html: getHtml,
+onclick: function(e) {
+var linkElm = editor.dom.getParent(e.target, 'a');
+
+if (linkElm) {
+editor.insertContent(
+'<img src="' + linkElm.getAttribute('data-mce-url') + '" alt="' + linkElm.getAttribute('data-mce-alt') + '" />'
+);
+
+//this.hide();
+}
+}
+},
+tooltip: 'Smile Skype',
+image: url + '/img/smile.jpg'
+});
+});
+
